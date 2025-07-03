17434fa3130ac7603f3385c60c19ed6dc9c33856
Juan Zhang <160123201+BelindaJZhang@users.noreply.github.com>
2025-07-03 15:33:48 +0000

    split train and main

--- day_1/duration_prediction/train.py
+++ day_1/duration_prediction/train.py
@@ -113,19 +113,6 @@ def train(train_date: date, val_date: date, out_path: str):
     return mse
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Train a linear regression model for trip duration prediction and save it to a given path.')
-    parser.add_argument('--train-date', required=True,  help='Training date in YYYY-MM format')
-    parser.add_argument('--val-date', required=True,  help='Validation date in YYYY-MM format')
-    parser.add_argument('--model-save-path', required=True,  help='Output path for the trained model')
-    args = parser.parse_args()
-    train_year, train_month = args.train_date.split('-')   
-    val_year, val_month = args.val_date.split('-')
 
-    train_date = date(int(train_year), int(train_month), 1)
-    val_date = date(int(val_year), int(val_month), 1)
-    out_path = args.model_save_path
-
-    train(train_date, val_date, out_path)
 
 
